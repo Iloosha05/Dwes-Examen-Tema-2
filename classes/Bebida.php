c9fa0bee62caa9fee30d51d8ee92c9c177f0ebf4
Iloosha05 <zilber.ilia@gmail.com>
2025-10-30 12:03:11 +0100

    Crear atributos necesarios en la clase bebida

--- classes/Bebida.php
+++ classes/Bebida.php
@@ -1,2 +1,11 @@
 <?php
 // Clase Bebida que hereda de Artículo
+
+class Bebida extends Articulo {
+    public $tamano;
+
+    public function __construct($nombre, $precio, $disponibilidad, $categoria, $tamano){
+        parent::__construct($nombre, $precio, $disponibilidad, $categoria);
+        $this->tamano = $tamano;
+    }
+}
